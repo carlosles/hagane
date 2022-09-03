449b1fa0358e18caa1d050966d8072b920a38da0
CarlosLes <CarlosLes@users.noreply.github.com>
2022-09-03 23:24:54 +0100

    add initial exploratory API using coroutines

---
+++ src/hagane/api_coroutines.py
@@ -0,0 +1,40 @@
+"""Exploratory API for coroutine-based event loop processing."""
+import asyncio
+from collections.abc import Awaitable
+
+
+async def park(car_aw: Awaitable[int]) -> int:
+    """Park car."""
+    car = await car_aw
+    print(f'{car}: parking car')
+    car += 5
+    print(f'{car}: finished parking car')
+    return car
+
+
+async def drive(car_aw: Awaitable[int]) -> int:
+    """Drive car."""
+    car = await car_aw
+    print(f'{car}: driving car')
+    car += 2
+    print(f'{car}: finished driving car')
+    return car
+
+
+async def make_car() -> int:
+    """Make car."""
+    car = 0
+    print(f'{car}: creating car')
+    print(f'{car}: finished creating car')
+    return car
+
+
+def main() -> None:  # noqa: D103
+    car_coro = make_car()
+    sim_coro = drive(park(car_coro))
+    result = asyncio.run(sim_coro)
+    print(f'{result}: simulation finished')
+
+
+if __name__ == '__main__':
+    main()
