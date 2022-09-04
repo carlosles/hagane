74032e9739529ac6b7ec7f7d61077aca07bb241c
CarlosLes <CarlosLes@users.noreply.github.com>
2022-09-04 23:17:22 +0100

    extend coroutine API with timeout scheduling

--- src/hagane/api_coroutines.py
+++ src/hagane/api_coroutines.py
@@ -1,39 +1,61 @@
 """Exploratory API for coroutine-based event loop processing."""
 import asyncio
 from collections.abc import Awaitable
+from dataclasses import dataclass
 
 
-async def park(car_aw: Awaitable[int], dt: int) -> int:
+@dataclass(slots=True)
+class Car:
+    """Class to represent car that tracks distance travelled."""
+
+    distance: int
+
+
+time = 0
+waiting: asyncio.PriorityQueue[int] = asyncio.PriorityQueue()
+
+
+async def timeout(dt: int) -> None:
+    """Time out for specified duration."""
+    global time
+    until = time + dt
+    await waiting.put(until)
+    while time < until:
+        time = await waiting.get()
+
+
+async def park(car_aw: Awaitable[Car], dt: int) -> Car:
     """Park car."""
     car = await car_aw
-    print(f'{car}: parking car')
-    car += dt
-    print(f'{car}: finished parking car')
+    print(f'{time=}: parking, {car}')
+    await timeout(dt)
+    car.distance += 1
+    print(f'{time=}: finished parking {car}')
     return car
 
 
-async def drive(car_aw: Awaitable[int], dt: int) -> int:
+async def drive(car_aw: Awaitable[Car], dt: int) -> Car:
     """Drive car."""
     car = await car_aw
-    print(f'{car}: driving car')
-    car += dt
-    print(f'{car}: finished driving car')
+    print(f'{time=}: driving, {car}')
+    await timeout(dt)
+    car.distance += 10
+    print(f'{time=}: finished driving {car}')
     return car
 
 
-async def make_car() -> int:
+async def make_car() -> Car:
     """Make car."""
-    car = 0
-    print(f'{car}: creating car')
-    print(f'{car}: finished creating car')
+    print(f'{time=}: creating car')
+    car = Car(distance=0)
+    print(f'{time=}: finished creating {car}')
     return car
 
 
 def main() -> None:  # noqa: D103
-    car_coro = make_car()
-    sim_coro = drive(park(car_coro, dt=5), dt=2)
+    sim_coro = drive(park(make_car(), dt=5), dt=2)
     result = asyncio.run(sim_coro)
-    print(f'{result}: simulation finished')
+    print(f'{time=}: simulation finished, {result}')
 
 
 if __name__ == '__main__':
