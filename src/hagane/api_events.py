2798320044caef94590493453f784e26eff8f65b
Carlos Les <les.charli@gmail.com>
2024-08-25 20:09:33 +0100

    fix events-based API recursion limit error

--- src/hagane/api_events.py
+++ src/hagane/api_events.py
@@ -2,48 +2,53 @@
 
 from collections.abc import Iterator
 from heapq import heappop, heappush
-from itertools import chain, count
+from itertools import chain, count, islice
 import sys
-
-from more_itertools import take
 
 
 type Process = Iterator[Event]
-type Event = tuple[float, int, Process]
+type Event = WaitEvent | ProcessEvent
+type WaitEvent = tuple[float, int]
+type ProcessEvent = tuple[float, int, Process]
 
-eids = count()
+eids = count(start=1)
 queue: list[Event] = []
 x = 0.0
 
 
-def run_sim(*processes):
+def run_sim(*processes: Process) -> Process:
     global queue, x
 
-    for process in processes:
-        heappush(queue, (0.0, next(eids), process))
+    for proc in processes:
+        heappush(queue, (x, next(eids), proc))
     while queue:
         event = heappop(queue)
         yield event
         x, _, process = event
-        next_x, next_eid, next_process = next(process)
-        next_event = (next_x, next_eid, chain(next_process, process))
-        heappush(queue, next_event)
+        try:
+            match next(process):
+                case (next_x, eid) as wait_event:  # A wait event.
+                    yield wait_event
+                    next_event = (next_x, next(eids), process)
+                case (next_x, eid, next_process):  # A process event.
+                    next_event = (next_x, eid, chain(next_process, process))
+                case invalid_event:
+                    raise ValueError(f'Invalid event {invalid_event}')
+            heappush(queue, next_event)
+        except StopIteration:
+            pass  # Do nothing.
 
 
-def defer_until(process: Process, until: float) -> Event:
+def defer_until(process: Process, until: float) -> ProcessEvent:
     return (until, next(eids), process)
 
 
-def defer_for(process: Process, dx: float) -> Event:
+def defer_for(process: Process, dx: float) -> ProcessEvent:
     return (x + dx, next(eids), process)
 
 
-def wait_for(dx: float) -> Event:
-    return (x + dx, next(eids), nothing())
-
-
-def nothing() -> Process:
-    yield from ()
+def wait_for(dx: float) -> WaitEvent:
+    return (x + dx, next(eids))
 
 
 def run_car() -> Process:
@@ -68,7 +73,7 @@ def park_car() -> Process:
 def main(*args):
     n_events = int(args[1]) if len(args) > 1 else 10
     events = run_sim(run_car())
-    for event in take(n_events, events):
+    for event in islice(events, n_events):
         print(event)
 
 
